de3564014be64dcbae8ac68e99a97dff9372111e
2200030048 <162856250+2200030048@users.noreply.github.com>
2024-12-07 09:02:08 +0530

    Add files via upload

---
+++ endsemexam/src/main/java/com/klef/jfsd/exam/service/CustomerService.java
@@ -0,0 +1,30 @@
+package com.klef.jfsd.exam.service;
+
+import com.klef.jfsd.exam.model.Customer;
+import com.klef.jfsd.exam.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Optional<Customer> updateCustomer(Long id, String name, String address) {
+        Optional<Customer> customerOptional = customerRepository.findById(id);
+        if (customerOptional.isPresent()) {
+            Customer customer = customerOptional.get();
+            customer.setName(name);
+            customer.setAddress(address);
+            customerRepository.save(customer);
+            return Optional.of(customer);
+        }
+        return Optional.empty();
+    }
+}
